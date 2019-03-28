45a74ce7d00b864cd2b394b4fe3c126e8617af90
Chris <cconover@gmail.com>
2019-03-28 16:50:51 -0700

    Fix error in modulus calculation.

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,8 +4,8 @@
         "gradient-1", "gradient-2", "gradient-3", "gradient-4", "gradient-5",
         "gradient-6", "gradient-7", "gradient-8", "gradient-9"]
     const sections = document.getElementsByTagName("section");
+    if (sections.length == 0) { return }
     for (var i = 0; i < sections.length; i++) {
-        const section = sections[i]
-        section.classList.add("section", gradients[i % sections.length])
+        sections[i].classList.add("section", gradients[i % gradients.length])
     }
 }()
